75892cefabb84bd9529b926145e7ead2fc4437ae
Yahia Galal <116451886+yahyagalal@users.noreply.github.com>
2024-07-01 21:33:54 +0300

    Update app.py
    fixed 10

--- app.py
+++ app.py
@@ -325,10 +325,10 @@ df['In Cairo'] = df.apply(lambda row: True if any(row[cairo_cities]) else False,
 
 #print(df.columns)
 
-print(df)
+st.write(df)
 df_scaled=scaler.transform(df)
 
-print(df_scaled)
+st.write(df_scaled)
 
 if st.button("Predict Price"):
     st.write("Price is ",int(model.predict(df_scaled)), " EGP")
